98a04a0fca1f31f3d0df69116dec4c11792cb836
ChUngHEe0227 <rogo0227@naver.com>
2021-09-09 16:54:58 +0900

    comint

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <thread>
 #include <vector>
+#include <cassert> 
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
@@ -20,6 +21,32 @@ using std::vector;
 constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
+
+
+void glob_to_local(Eigen::VectorXd x_glob, Eigen::VectorXd y_glob, 
+                   double psi, double px, double py, 
+                   Eigen::VectorXd &x_local, Eigen::VectorXd &y_local){
+  
+  double cc = cos(psi);
+  double ss = sin(psi);
+  double x_local_, y_local_;
+  double x_diff_glob, y_diff_glob;
+
+  for(int i=0; i<x_glob.size(); i++){
+    x_diff_glob = x_glob[i] - px;
+    y_diff_glob = y_glob[i] - py;
+
+    x_local_ = x_diff_glob * cc + y_diff_glob * ss;
+    y_local_ = - x_diff_glob * ss + y_diff_glob * cc;
+
+    x_local[i] = x_local_;
+    y_local[i] = y_local_;
+  }
+
+
+}
+
+
 
 int main() {
   uWS::Hub h;
@@ -47,14 +74,50 @@ int main() {
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
+          double steering_angle = j[1]["steering_angle"];
+          double throttle = j[1]["throttle"];
 
+          double throttle_real;
+          if (throttle > 0){
+            throttle_real = throttle * 5;
+          }else{
+            throttle_real = throttle * 12;
+          }
+          double* ptsx_ptr = &ptsx[0];
+          Eigen::Map<Eigen::VectorXd> ptsx_(ptsx_ptr, ptsx.size());
+          
+          double* ptsy_ptr = &ptsy[0];
+          Eigen::Map<Eigen::VectorXd> ptsy_(ptsy_ptr, ptsy.size());
+          
+          Eigen::VectorXd ptsx_local(ptsx.size());
+          Eigen::VectorXd ptsy_local(ptsy.size()); 
+          glob_to_local(ptsx_, ptsy_, psi, px, py, ptsx_local, ptsy_local);
           /**
            * TODO: Calculate steering angle and throttle using MPC.
            * Both are in between [-1, 1].
            */
+          Eigen::VectorXd coeffs = polyfit(ptsx_local, ptsy_local, 3);
+          double d_ahead = v * 0.15; 
+          double cte = polyeval(coeffs, d_ahead);
+          double epsi = - atan(coeffs[1]);
+          
+        
+          Eigen::VectorXd state(6+2);
+          state << d_ahead, 0, 0, v, cte, epsi, -steering_angle, throttle_real;
+
+
+          // solve MPC!
+          auto vars = mpc.Solve(state, coeffs);
+
           double steer_value;
           double throttle_value;
 
+          steer_value = - vars[0] / 2.67; // normalize to [-1, 1]
+          if(vars[1] >= 0){
+            throttle_value = vars[1] / 5;
+          }else{
+            throttle_value = vars[1] / 12;
+          }
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the 
           //   steering value back. Otherwise the values will be in between 
